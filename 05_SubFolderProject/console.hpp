6b6a85c089d14b071a0f6ca5f3b1448ceff615ca
lsrinivasamurthy <lsrinivasamurthy@users.noreply.github.com>
2024-02-21 18:47:47 +0530

    Add files via upload

---
+++ 05_SubFolderProject/console.hpp
@@ -0,0 +1,11 @@
+//
+// Created by lkk5kor on 20/2/24.
+//
+
+#ifndef SUBFOLDERPROJECT_CONSOLE_HPP
+#define SUBFOLDERPROJECT_CONSOLE_HPP
+
+#include "include/Input.hpp"
+#include "include/Output.hpp"
+
+#endif //SUBFOLDERPROJECT_CONSOLE_HPP
