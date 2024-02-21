79d6a84702c89a8d7c8aa118549f8a89db3505bf
lsrinivasamurthy <lsrinivasamurthy@users.noreply.github.com>
2024-02-21 18:43:12 +0530

    Add files via upload

---
+++ Output.hpp
@@ -0,0 +1,15 @@
+//
+// Created by lkk5kor on 20/2/24.
+//
+
+#ifndef SUBFOLDERPROJECT_OUTPUT_HPP
+#define SUBFOLDERPROJECT_OUTPUT_HPP
+
+#include "string"
+
+namespace console
+{
+    void writeString(::std::string str);
+}
+
+#endif //SUBFOLDERPROJECT_OUTPUT_HPP
